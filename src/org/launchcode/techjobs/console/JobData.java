7718e2c6a7ce13611ee163fd99f58422fa8f8d87
Deb Mendoza <debmendoza13@gmail.com>
2018-12-13 20:53:23 -0600

    Completed assignment, printJobs, findByAll, case insensitive - done

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -62,7 +63,7 @@ public class JobData {
      * with "Enterprise Holdings, Inc".
      *
      * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param value Value of the field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -73,14 +74,34 @@ public class JobData {
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
 
         for (HashMap<String, String> row : allJobs) {
-
-            String aValue = row.get(column);
+            String original_value = row.get(column);
+            String aValue = original_value.toLowerCase();
 
             if (aValue.contains(value)) {
                 jobs.add(row);
             }
         }
 
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            for (Map.Entry<String, String> cell : row.entrySet()) {
+                String cellValue = cell.getValue();
+                cellValue = cellValue.toLowerCase();
+                value = value.toLowerCase();
+
+                if (cellValue.contains(value)) {
+                    jobs.add(row);
+                    break;
+                }
+            }
+        }
         return jobs;
     }
 
